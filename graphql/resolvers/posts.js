cbcbd0935f4f7ea24944a8c2d0aed61162b4cd53
Evan <ewpw5678@gmail.com>
2021-12-18 11:27:48 -0800

    feat: delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require('apollo-server');
+
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -38,6 +40,21 @@ module.exports = {
       const post = await newPost.save();
 
       return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return 'Post deleted successfully';
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
