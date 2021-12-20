ade084a3a643a65411d4f1bf0c0a5930009be9a5
Evan Walter <ewpw5678@gmail.com>
2021-12-19 17:11:26 -0800

    style: added tailwind

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '../index.css';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
