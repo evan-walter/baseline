457f4c0c94a14005ebc4e4d93f4fd19c6c33e589
Evan Walter <ewpw5678@gmail.com>
2021-12-30 17:14:48 -0800

    feat: a working responsive navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
+  const [barsExpanded, setBarsExpanded] = useState(false);
+
+  function handleBars() {
+    setBarsExpanded(s => !s);
+  }
+
+  function closeBars() {
+    setBarsExpanded(false);
+  }
+
+  const navLink = 'py-2 px-4 hover:bg-gray-200 hover:rounded-3xl';
+
   return (
-    <nav className='flex items-center py-4 px-8 bg-bglight text-black'>
-      <div className=''>
-        <NavLink to='/' className='text-5xl font-black'>b</NavLink>
+    <nav className='block lg:flex lg:items-center lg:justify-between py-4 px-8 bg-bglight text-black'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center'>
+          <NavLink to='/' className='text-5xl font-black'>b</NavLink>
+        </div>
+        <div className='lg:hidden'>
+          <button className='text-black' onClick={handleBars}>
+            <svg className='h-6 w-6 fill-current' viewBox='0 0 24 24'>
+              {barsExpanded ? <path d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'/> :
+                <path d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'/>}
+            </svg>
+          </button>
+        </div>
       </div>
-      <div className=''>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/register'>Register</NavLink>
-        <NavLink to='/login'>Login</NavLink>
+      <div className={(barsExpanded ? 'flex flex-col' : 'hidden lg:flex') + ' text-center'}>
+        <NavLink className={navLink} to='/' onClick={closeBars}>Home</NavLink>
+        <NavLink className={navLink} to='/register' onClick={closeBars}>Register</NavLink>
+        <NavLink className={navLink} to='/login' onClick={closeBars}>Login</NavLink>
       </div>
     </nav>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       colors: {
         textlight: '#e6e6e6',
         btnprimary: '#c31432',
-        bgprimary: '#000',
+        bgprimary: '#111',
         bgsecondary: '#212327', // inputs
         bglight: '#fefefe', // navbar
       },
