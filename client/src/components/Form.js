43fac341b14284bb0541abd3d33d78e726cef0b1
Evan Walter <ewpw5678@gmail.com>
2022-01-02 22:24:51 -0800

    style: nice auths

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -16,14 +16,14 @@ export default function Form({ register }) {
     e.preventDefault();
   }
 
-  const item = 'my-2 flex flex-col';
-  const label = 'my-1';
-  const input = ' my-1 p-2 text-textlight bg-bgsecondary focus:outline-none focus:ring focus:ring-themeprimary rounded';
+  const item = 'py-2 flex flex-col';
+  const label = 'py-1';
+  const input = ' my-1 p-2 text-textlight bg-bgprimary border border-bordergray focus:outline-none focus:ring focus:ring-themeprimary rounded';
 
   return (
     <div className='flex justify-center border border-bordergray'>
-      <div>
-        <h1 className='mb-2 text-lg'>{register ? 'Register' : 'Login'}</h1>
+      <div className='w-full max-w-xl py-8 px-24 bg-bgprimary rounded-xl'>
+        <h1 className='py-4 text-xl'>{register ? 'Register' : 'Login'}</h1>
         <form onSubmit={onSubmit}>
           <div className={item}>
             <label htmlFor='username' className={label}>Username</label>
@@ -33,19 +33,19 @@ export default function Form({ register }) {
           {register ? (
             <div className={item}>
               <label htmlFor='email' className={label}>Email</label>
-              <input id='email' type='text' maxLength='50' required name='email' placeholder='Email'
+              <input id='email' type='email' maxLength='50' required name='email' placeholder='Email'
                 value={values.email} onChange={onChange} className={input} />
             </div>
           ) : null}
           <div className={item}>
             <label htmlFor='password' className={label}>Password</label>
-            <input id='password' type='text' maxLength='50' required name='password' placeholder='Password'
+            <input id='password' type='password' maxLength='50' required name='password' placeholder='Password'
               value={values.password} onChange={onChange} className={input} />
           </div>
           {register ? (
             <div className={item}>
               <label htmlFor='confirmPassword' className={label}>Confirm Password</label>
-              <input id='confirmPassword' type='text' maxLength='50' required name='confirmPassword' placeholder='Confirm Password'
+              <input id='confirmPassword' type='password' maxLength='50' required name='confirmPassword' placeholder='Confirm Password'
                 value={values.confirmPassword} onChange={onChange} className={input} />
             </div>
           ) : null}
